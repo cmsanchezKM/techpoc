6fe19acafed8983c45398a54e4e3c320b3d98e4e
cmsanchez <cmsanchez@knowmadmood.com>
2026-07-15 09:12:18 +0200

    feat(posts): add posts module with list, detail and form components

---
+++ src/app/features/posts/form/post-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-form',
+  imports: [],
+  templateUrl: './post-form.component.html',
+  styleUrl: './post-form.component.css',
+})
+export class PostForm {}
